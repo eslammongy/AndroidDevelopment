1d6141b85663f817eb2e674f2a52addb6cc0b757
eslam <eslammongy995@gmail.com>
2022-12-18 11:25:37 +0200

    build login screen

--- app/src/main/java/com/eslam/shoestoreapp/MainActivity.kt
+++ app/src/main/java/com/eslam/shoestoreapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.eslam.shoestoreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.eslam.shoestoreapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfig:AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfig = AppBarConfiguration(navController.graph)
+        val toolbar = binding.mainToolbar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfig)
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfig)
     }
 }
